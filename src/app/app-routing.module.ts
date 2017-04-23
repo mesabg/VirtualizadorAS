5f1a51c0a98bf10e94f60db68c8b42811b224ba7
Moises Berenguer <moises.berenguer@gmail.com>
2017-04-22 21:32:49 -0400

    Thrid Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Ng2PageTransitionModule } from "ng2-page-transition";
 
-const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
-];
+//-- Importing Pages
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+const routes: Routes = [{
+	path: '',
+	children: [
+		{path: '', redirectTo: 'inicio', pathMatch: 'full'},
+		{path: 'inicio', component: InicioComponent},
+		{path:'**', redirectTo: 'inicio'}
+	]
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+  	RouterModule.forRoot(routes),
+  	Ng2PageTransitionModule
+  ],
+  exports: [
+  	RouterModule,
+  	Ng2PageTransitionModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
